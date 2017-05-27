3b70f931f4e35051e0d89ae1c6bdadb510d94461
Timothy Dawson <timltdawson@gmail.com>
2017-05-27 16:10:31 -0600

    _blank is back with routes working on server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 http.listen(8000, function(){
     console.log("server running on port 8000");
 })
