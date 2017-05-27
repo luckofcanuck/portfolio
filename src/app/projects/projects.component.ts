921cee6545d594554eebe8f728d2ce40b27c4168
Timothy Dawson <timltdawson@gmail.com>
2017-05-27 16:24:07 -0600

    using hashes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -29,7 +29,7 @@ export class ProjectsComponent implements OnInit {
           subtitle: "A CSS styling and positioning showcase extravaganza.",
           description: "This project was an exercise in conversion. KandJ Diaries is an archived out-of-use website that I rewrote from scratch in order to showcase my abilities in converting PSDs, wireframes, or, in this case, another webpage. I can clean things up, make them functional, and turn whatever vision a client has into a reality.",
           img: "/assets/img/jumboK.png",
-          url: "/kandj"
+          url: "/#/kandj"
       }
   ]
    constructor() { }
