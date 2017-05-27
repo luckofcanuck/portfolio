2e9a6bb860e5f1bcaa71affe6bcb6e14046700e6
Timothy Dawson <timltdawson@gmail.com>
2017-05-27 14:43:43 -0600

    running on server

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var morgan = require("morgan");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var config = require("./config");
-var expressJwt = require("express-jwt");
 var http = require("http").Server(app);
-var io = require("socket.io")(http);
 var port = process.env.PORT || 8000;
 
+app.use(express.static(path.join(__dirname, "dist")));
 
 http.listen(8000, function(){
     console.log("server running on port 8000");
